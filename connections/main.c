0eab28dd9c1173acb6373d54acdc33f5015c2713
Iggy Villar <Ivillar2379@conestogac.on.ca>
2024-07-25 15:33:48 -0400

    added main loop

--- connections/main.c
+++ connections/main.c
@@ -14,7 +14,7 @@
 #define FILE "connectionsData.txt"
 
 int main(void) {
-	// randomize by time
+	/*// randomize by time
 	srand(time(NULL));
 
 	// initialize head of linked list
@@ -45,6 +45,67 @@ int main(void) {
 
 	// delete and free nodes from linked list
 	deleteNode(head);
+	*/
+    // Randomize by time
+    srand(time(NULL));
 
-	return 0;
+    // Create pointer array of connections
+    CONNECTION* connectionArr[TOTALCONNECTIONS];
+
+    // Read file data and load data into connection array
+    if (!loadData(FILE, connectionArr)) {
+        // Close program with error
+        exit(EXIT_FAILURE);
+    }
+
+    NODE* head = NULL;
+    GAME_STATE gameState;
+    HIGHSCORE highScores;
+    int choice;
+
+    do {
+        choice = displayMenu();
+        switch (choice) {
+        case 1:
+            // Allocate memory for the head node
+            head = (NODE*)malloc(sizeof(NODE));
+            if (head == NULL) {
+                fprintf(stderr, "Error allocating memory for head node\n");
+                exit(EXIT_FAILURE);
+            }
+
+            // Create linked list
+            createList(head, connectionArr);
+
+            // Initialize and start the game
+            initializeGame(&gameState, head, &highScores);
+            startGame(&gameState, &highScores);
+
+            // Free the linked list after the game ends
+            deleteNode(head);
+            free(head);
+            head = NULL; // Set head to NULL after freeing
+            break;
+
+        case 2:
+            // Print highscores (implement this function as needed)
+            // printHighscores(highscore);
+            break;
+
+        case 3:
+            printf("Exiting game. See you soon!!!\n");
+            break;
+
+        default:
+            printf("Invalid choice. Please try again.\n");
+            break;
+        }
+    } while (choice != 3);
+
+    // Free memory allocated for connections
+    for (int i = 0; i < TOTALCONNECTIONS; i++) {
+        free(connectionArr[i]);
+    }
+
+    return 0;
 }
