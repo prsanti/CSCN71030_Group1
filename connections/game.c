f8a2f34ce501bcfcbbadbe536b3a0cd63595d71c
Karl Santos <karlxsantos@gmail.com>
2024-07-25 16:19:17 -0400

    freak

--- connections/game.c
+++ connections/game.c
@@ -5,7 +5,7 @@
 void initializeGame(GAME_STATE* gameState, NODE* head, HIGHSCORE* highscore)
 {
     gameState->head = head; // set the connections
-    strcpy(gameState->player.name, "karl");	// set the name
+    strcpy(gameState->player.name, "freak");	// set the name
     gameState->player.score = 0;	// set the score
     gameState->lives = MAX_LIVES; // set the lives
 
