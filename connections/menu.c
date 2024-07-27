f41c6c181eab5a6524c84e0eb138572d93831fa9
Karl Santos <karlxsantos@gmail.com>
2024-07-27 01:53:27 -0400

    Revert "fixed displayMenu() input"
    This reverts commit be58225ffa320cd0c1ff3313d67db08ef47efb0f.

--- connections/menu.c
+++ connections/menu.c
@@ -4,21 +4,13 @@
 
 int displayMenu() {
     int choice;
-    char inputBuffer[100];
     printf("Welcome to Connections\n");
     printf("Please proceed by entering the number of the option you wish to execute\n");
     printf("1) Start Game\n");
     printf("2) Show Highscores\n");
     printf("3) Exit\n");
     printf("Enter choice: ");
-    // Use fgets to get input and handle the buffer
-    if (fgets(inputBuffer, sizeof(inputBuffer), stdin) != NULL) {
-        // Convert the input to an integer
-        choice = atoi(inputBuffer); // atoi returns 0 if input is not a valid integer
-    }
-    else {
-        choice = 0; // Default value if input failed
-    }
+    scanf("%d", &choice);
 
     return choice;
 }
